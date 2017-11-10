37a25e535e46af3600510a3d689204aa3b04b58a
Daniel Bernstein <dabylon@gmail.com>
2017-11-10 11:41:22 -0800

    updates version to 0.3.1

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='milldeploy',
-    version='0.4.0-snapshot',
+    version='0.3.1',
     py_modules=['milldeploy'],
     install_requires=[
         'Click',
