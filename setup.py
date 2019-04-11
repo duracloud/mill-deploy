3a501c104a4d7b8098b7a3c6e047b6218d6cd144
Daniel Bernstein <dabylon@gmail.com>
2019-04-10 17:02:44 -0700

    updates version for continued dev.

--- setup.py
+++ setup.py
@@ -3,7 +3,7 @@ from setuptools import setup
 
 setup(
     name='milldeploy',
-    version='0.4.0',
+    version='0.5.0-snapshot',
     py_modules=['milldeploy'],
     install_requires=[
         'Click',
